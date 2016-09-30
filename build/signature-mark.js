f6827ca05388eeaff75b6e90bcd460324eb029b0
touhonoob <touhonoob@gmail.com>
2016-10-01 03:46:21 +0800

    add destroy() method

--- build/signature-mark.js
+++ build/signature-mark.js
@@ -1,4 +1,4 @@
-/*! signature-mark.js - 0.0.1 - 2014-10-12 - motdotla */
+/*! signature-mark.js - 0.0.1 - 2016-10-01 - motdotla */
 (function(exports){
   var SignatureMark = function(canvas) {
     if(!(this instanceof SignatureMark)){
@@ -22,14 +22,49 @@
 }(this));
 
 (function(SignatureMark){
-  SignatureMark.prototype.initEvents = function() {
-    var self = this;
-    self.canvas.addEventListener(self.mouse_down, function(e)     { self.onCanvasMouseDown(self, e); }, false);
-    self.canvas.addEventListener(self.mouse_move, function(e)     { self.onCanvasMouseMove(self, e); }, false);
-    self.canvas.addEventListener('contextmenu', function(e)       { self.preventRightClick(self, e); }, false);
+  var listeners = {};
 
-    document.addEventListener(self.mouse_up, function(e)          { self.onCanvasMouseUp(self, e); }, false);
-    self.canvas.addEventListener(self.mouse_up, function(e)       { self.onCanvasMouseUp(self, e); }, false);  
+  SignatureMark.prototype.initEvents = function () {
+    var self = this;
+    listeners = {
+      canvas: {},
+      document: {}
+    };
+    listeners.canvas[self.mouse_down] = function (e) {
+      self.onCanvasMouseDown(self, e);
+    };
+    listeners.canvas[self.mouse_move] = function (e) {
+      self.onCanvasMouseMove(self, e);
+    };
+    listeners.canvas.contextmenu = function (e) {
+      self.preventRightClick(self, e);
+    };
+    listeners.canvas[self.mouse_up] = function (e) {
+      self.onCanvasMouseUp(self, e);
+    };
+    listeners.document[self.mouse_up] = function (e) {
+      self.onCanvasMouseUp(self, e);
+    };
+
+    for (var canvasEvent in listeners.canvas) {
+      self.canvas.addEventListener(canvasEvent, listeners.canvas[canvasEvent], false);
+    }
+
+    for (var documentEvent in listeners.document) {
+      document.addEventListener(documentEvent, listeners.document[documentEvent], false);
+    }
+  };
+
+  SignatureMark.prototype.destroy = function() {
+    if (self.canvas) {
+      for (var canvasEvent in listeners.canvas) {
+        self.canvas.removeEventListener(canvasEvent, listeners.canvas[canvasEvent]);
+      }
+    }
+
+    for (var documentEvent in listeners.canvas) {
+      document.removeEventListener(documentEvent, listeners.document[documentEvent]);
+    }
   };
 
   SignatureMark.prototype.preventRightClick = function(self, e) {
--- src/signature-mark/events.js
+++ src/signature-mark/events.js
@@ -1,12 +1,47 @@
 (function(SignatureMark){
-  SignatureMark.prototype.initEvents = function() {
-    var self = this;
-    self.canvas.addEventListener(self.mouse_down, function(e)     { self.onCanvasMouseDown(self, e); }, false);
-    self.canvas.addEventListener(self.mouse_move, function(e)     { self.onCanvasMouseMove(self, e); }, false);
-    self.canvas.addEventListener('contextmenu', function(e)       { self.preventRightClick(self, e); }, false);
+  var listeners = {};
 
-    document.addEventListener(self.mouse_up, function(e)          { self.onCanvasMouseUp(self, e); }, false);
-    self.canvas.addEventListener(self.mouse_up, function(e)       { self.onCanvasMouseUp(self, e); }, false);  
+  SignatureMark.prototype.initEvents = function () {
+    var self = this;
+    listeners = {
+      canvas: {},
+      document: {}
+    };
+    listeners.canvas[self.mouse_down] = function (e) {
+      self.onCanvasMouseDown(self, e);
+    };
+    listeners.canvas[self.mouse_move] = function (e) {
+      self.onCanvasMouseMove(self, e);
+    };
+    listeners.canvas.contextmenu = function (e) {
+      self.preventRightClick(self, e);
+    };
+    listeners.canvas[self.mouse_up] = function (e) {
+      self.onCanvasMouseUp(self, e);
+    };
+    listeners.document[self.mouse_up] = function (e) {
+      self.onCanvasMouseUp(self, e);
+    };
+
+    for (var canvasEvent in listeners.canvas) {
+      self.canvas.addEventListener(canvasEvent, listeners.canvas[canvasEvent], false);
+    }
+
+    for (var documentEvent in listeners.document) {
+      document.addEventListener(documentEvent, listeners.document[documentEvent], false);
+    }
+  };
+
+  SignatureMark.prototype.destroy = function() {
+    if (self.canvas) {
+      for (var canvasEvent in listeners.canvas) {
+        self.canvas.removeEventListener(canvasEvent, listeners.canvas[canvasEvent]);
+      }
+    }
+
+    for (var documentEvent in listeners.canvas) {
+      document.removeEventListener(documentEvent, listeners.document[documentEvent]);
+    }
   };
 
   SignatureMark.prototype.preventRightClick = function(self, e) {
